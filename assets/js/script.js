32b7278c3df38385c70d81d56fa4dcb6a043dc2d
michellemb306 <michellemirandab@gmail.com>
2020-08-30 21:12:16 -0400

    Añadi gradico y otros detalles

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,6 +2,7 @@
 $(function () {
    $("#buscar").click(e=>{
       buscarPersonaje();
+     
    });
 
    $(document).keypress(e => { 
@@ -20,6 +21,8 @@ traerPersonaje = (id) =>{
             //imprimir data
                 $("#pokemon-card").empty(generarCard(response));
                 $("#pokemon-card").append(generarCard(response));  
+               
+              
         }
     });
 }
@@ -43,9 +46,46 @@ generarCard = (personaje) => {
         </div>
       </div>
     </div>`
+    //Grafico
+    var hp =  personaje.stats[0].base_stat;
+    var atk = personaje.stats[1].base_stat;
+    var def = personaje.stats[2].base_stat;
+    var sat = personaje.stats[3].base_stat;
+    var sdf = personaje.stats[4].base_stat;
+    var spd = personaje.stats[5].base_stat;
+    var pokest = [];
+     pokest.push(hp, atk, def, sat, sdf, spd);
+     
 
+
+      var chart = new CanvasJS.Chart("pokemon-grafico", {
+        theme: "light1", // "light2", "dark1", "dark2"
+        animationEnabled: false, // change to true		
+        title:{
+          text: "Gráfico Pokémon"
+        },
+        data: [
+        {
+          // Change type to "bar", "area", "spline", "pie",etc.
+          type: "column",
+          dataPoints: [
+            { label: "HP",  y:parseFloat(hp)  },
+            { label: "Ataque", y: parseFloat(atk)  },
+            { label: "Defensa", y: parseFloat(def) },
+            { label: "Ataque Especial",  y: parseFloat(sat) },
+            { label: "Defensa Especial",  y: parseFloat(sdf)  },
+            { label: "Velocidad",  y: parseFloat(spd)  }
+          ]
+        }
+        ]
+      });
+      chart.render();
     return card;
-}
+};
+
+
+ 
+ 
 
   validacion = (id) =>{
     var expresion = /^\d{1,3}$/;
@@ -63,8 +103,16 @@ generarCard = (personaje) => {
         //validacion
         if(validacion(idPokemon) || idPokemon<807 || idPokemon>0){
           traerPersonaje(idPokemon);
+         
           $("#poke-id").val("");
           $("#poke-id").focus();
           
         }
   }
+
+
+   
+  
+  
+
+  
